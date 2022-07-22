07dbbbffe0a63b237779ea48b49fdec79e88149b
kushal <kushal.shah1@ibm.com>
2022-07-22 12:32:40 +0530

    first source code commit.

---
+++ SampleApp/SampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppApp.swift
+//  SampleApp
+//
+//  Created by 003x9k on 22/07/22.
+//
+
+import SwiftUI
+
+@main
+struct SampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
